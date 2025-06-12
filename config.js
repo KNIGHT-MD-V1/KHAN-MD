3924c8cfa549d44220ad9325b3400646067e7fa8
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-06-12 11:28:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "malvin~p6t2yTQB#-AJOi2DerD2mahHPjGp0Pv62vOKCAumgQaIGti3LzX8",
+SESSION_ID: process.env.SESSION_ID || "IK~p6t2yTQB#-AJOi2DerD2mahHPjGp0Pv62vOKCAumgQaIGti3LzX8",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
